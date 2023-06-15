0271ca3aa7e1c0e721d348213659ac5aefb19646
elvis <elvismulaj12@gmail.com>
2023-06-15 21:47:17 +0200

    login and registration
    login and registration

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require ("express");
 const app=express();
+const mongoose =require("mongoose");
+const dotenv=require("dotenv");
+const authRoute= require("./routes/auth")
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL ,{
+    useNewUrlParser:true,
+}).then(() =>console.log("db connection successfull"))
+.catch((err)=> console.log(err));
+
+app.use(express.json())
+
+
+app.use("/api/auth",authRoute);
 
 
 app.listen(8800, () =>{
     console.log("backend is runing")
-})
+}) 
---
+++ routes/auth.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const User = require ("../models/User");
+const  CryptoJS = require("crypto-js");
+const jwt=require ("jsonwebtoken");
+
+
+//regjstrimi
+
+
+router.post("/register",async (req,res) =>{
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password,process.env.SECRET_KEY).toString(),
+    });
+
+    try{
+
+//dergimi ne databaze
+
+
+const user = await newUser.save();
+res.status(201).json(user);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
+
+//LOGIMI
+
+router.post("/login", async (req,res) =>{
+    try{
+
+        //kushtet pa if
+      const user= await User.findOne({email:req.body.email});
+        !user && res.status(401).json("wrong password or username");
+//perdorimi i cryjs per kriptim te passwordave
+        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword= bytes.toString(CryptoJS.enc.Utf8);
+//perdorimii  webtokenave
+
+
+        originalPassword !== req.body.password && res.status(401).json("wrong password or username");
+
+        //desktruktorimi i passwordit
+
+        const {password,...info}=user._doc;
+
+        res.status(200).json(info);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+
+module.exports = router;
