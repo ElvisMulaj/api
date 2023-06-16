200725fd7030ddee8955ddacf396935d6ecd3c71
elvis <elvismulaj12@gmail.com>
2023-06-16 16:15:43 +0200

    end of cruds operations
    end of cruds operations

--- models/List.js
+++ models/List.js
@@ -5,7 +5,7 @@ const ListSchema= new mongoose.Schema({
     title:{type:String, require:true, unique:true},
     type:{type:String},
     genre:{type:String},
-    contenct:{type:Array}
+    content:{type:Array}
 },{
     timestamps:true
 });
